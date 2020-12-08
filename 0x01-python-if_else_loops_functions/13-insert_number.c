faecf7417b8aa46bf37c5179d7caa56110a44ccb
ezzedin779 <abdou.babchia64@gmail.com>
2020-12-08 23:09:49 +0100

    in sorted list ?

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -20,15 +20,9 @@ listint_t *insert_node(listint_t **head, int number)
 		*head = new;
 		return (new);
 	}
-	while (actual && actual->next)
-	{
-		if (actual->next->n > number)
-			actual = actual->next;
-		else
-		{
-			new->next = actual->next;
-			actual->next = new;
-		}
-	}
+	while (actual && actual->next && actual->next->n > number)
+		actual = actual->next;
+	new->next = actual->next;
+	actual->next = new;
 	return (new);
 }
