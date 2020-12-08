6615e6e5e817928ff52b41ea47e2b81a54d30155
ezzedin779 <abdou.babchia64@gmail.com>
2020-12-08 23:03:09 +0100

    in sorted list ?

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
 *insert_node - inserting node in a sorted way
-*@head: head of the list
+*@head: actual of the list
 *@number: content of the new node
 *Return: the adress of the new node
 */
@@ -9,25 +9,26 @@ listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *actual, *new;
 
-	*actual = *head;
+	actual = *head;
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
 	new->n = number;
-	if (head == NULL || head->n >= number)
+	if (actual == NULL || actual->n >= number)
 	{
-		new->next = head;
-		*head = new;
+		new->next = actual;
+		*actual = *new;
 		return (new);
 	}
 	while (actual && actual->next)
 	{
-		if (actual->next->n > number)
+		if (actual->next->n < number)
 			actual = actual->next;
 		else
 		{
 			new->next = actual->next;
 			actual->next = new;
+			break;
 		}
 	}
 	return (new);
