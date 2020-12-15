e5c2f5c509e049f851b6df732f1bfc6be25fd0f4
ezzedin779 <abdou.babchia64@gmail.com>
2020-12-15 11:16:29 +0100

    return a lot ?

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if len(sentence) == 0:
+        c = (0, None)
+        return c
+    else:
+        c = (len(sentence), sentence[0])
+        return c
