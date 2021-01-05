ced9d4dec985c19f4343d4de0ed84f74e7fb12b0
ezzedin779 <abdou.babchia64@gmail.com>
2021-01-05 20:27:56 +0100

    simpliest square

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Defining a Square class contains The size ? """
+
+
+class Square:
+    """ New item SIZEEEE """
+    def __init__(self, size=0):
+        """ Initialize the size ? please ?
+        Do verification also please"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
