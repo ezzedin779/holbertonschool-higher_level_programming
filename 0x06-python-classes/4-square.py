132977d9119da056b3d8399d51b4f1a69229093e
ezzedin779 <abdou.babchia64@gmail.com>
2021-01-05 20:56:34 +0100

    SET GET RETRIEVEEE

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -8,7 +8,7 @@ class Square:
     def __init__(self, size=0):
 	    """ Initialize the size ? please ?
         Do verification also please"""
-        self.size = size
+    pass
 
     @property
     def size(self):
