75500fa664a18a3253c61e28e74f8bf129d36c27
ezzedin779 <abdou.babchia64@gmail.com>
2021-01-05 20:27:46 +0100

    more simple square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Defining a Square class contains The size ? """
+
+
+class Square:
+    """ New item SIZEEEE """
+    def __init__(self, size):
+        """ Initialize the size ? please ?"""
+        self.__size = size
