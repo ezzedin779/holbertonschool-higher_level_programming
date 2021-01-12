031498d956fe2db8bc52e43558e0f0766ce19199
ezzedin779 <abdou.babchia64@gmail.com>
2021-01-12 08:47:25 +0100

    that symbol please ?

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -59,7 +59,7 @@ class Rectangle:
         for i in range(self.__height):
             for j in range(self.width):
                 rect += str(self.print_symbol)
-            if i < self.__height:
+            if i < self.__height - 1:
                 rect += "\n"
         return rect
 
