bce9331b00274ef603910c67ac2b63dba3617001
ezzedin779 <abdou.babchia64@gmail.com>
2021-01-20 20:57:37 +0100

    load from a file json

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+""" loading from a file ?"""
 import json
+
+
 def load_from_json_file(filename):
+    """ Python object"""
     with open(filename) as f:
         return json.load(f)
