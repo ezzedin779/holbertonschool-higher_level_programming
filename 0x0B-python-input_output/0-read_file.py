ca0ebc5b8fd56cbc01ddfe0130156efce8b58513
ezzedin779 <abdou.babchia64@gmail.com>
2021-01-20 21:00:39 +0100

    READ

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+""" read a file """
+
+
 def read_file(filename=""):
+    """ what do u have in mind ?"""
     with open(filename, encoding='utf-8') as f:
         print(f.read())
