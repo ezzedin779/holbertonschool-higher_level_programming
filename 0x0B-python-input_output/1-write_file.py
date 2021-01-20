f3d36b72915a3ef88507087fa39eef3065311390
ezzedin779 <abdou.babchia64@gmail.com>
2021-01-20 21:00:47 +0100

    write

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+""" write in a file """
 def write_file(filename="", text=""):
+    """ write that """
     with open(filename, mode='w', encoding='utf-8') as f:
         return f.write(text)
