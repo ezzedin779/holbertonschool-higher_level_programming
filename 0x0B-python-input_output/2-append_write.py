b211d804bd6a0cc14b610112a22b3c7642f75651
ezzedin779 <abdou.babchia64@gmail.com>
2021-01-20 21:00:53 +0100

    append

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+""" appending in a file"""
+
+
 def append_write(filename="", text=""):
+    """ append """
     with open(filename, mode='a', encoding='utf-8') as f:
         return f.write(text)
