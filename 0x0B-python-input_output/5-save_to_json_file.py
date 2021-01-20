363f8a1890706c1b7eb3f0c1992240841706d53d
ezzedin779 <abdou.babchia64@gmail.com>
2021-01-20 21:01:27 +0100

    code to that file

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+""" save to json file"""
 import json
+
+
 def save_to_json_file(my_obj, filename):
+    """ Python object"""
     with open(filename, mode='w') as f:
         json.dump(my_obj, f)
