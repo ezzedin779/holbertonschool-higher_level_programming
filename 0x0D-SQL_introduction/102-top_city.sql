f58956124af85f4a3412f11eae18317d9f11314d
ezzedin779 <abdou.babchia64@gmail.com>
2021-03-01 20:56:11 +0100

    WHOS HOTTER ?

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,3 @@
+-- AVERAGE TEMPERATURE
+-- twenty task (Task 19)
+SELECT `city`, AVG(`value`) AS `avg_temp` FROM `temperatures` WHERE `month` = 7 OR `month` = 8 GROUP BY `city` ORDER BY `avg_temp` DESC LIMIT 3;
