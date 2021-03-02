5d5f542f5ab3ddbf3af439e9c0dc72090b181d31
ezzedin779 <abdou.babchia64@gmail.com>
2021-03-02 21:40:00 +0100

    shows by genre ?

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,10 @@
+-- ALL TV SHOWS BY THEIR GENRE 
+-- TASK 16
+SELECT tv_shows.`title`, tv_genres.`name`
+  FROM `tv_shows` AS tv_shows
+       LEFT JOIN `tv_show_genres` AS tv_show_genres
+       ON tv_shows.`id` = tv_show_genres.`show_id`
+
+       LEFT JOIN `tv_genres` AS tv_genres
+       ON tv_show_genres.`genre_id` = tv_genres.`id`
+ ORDER BY tv_shows.`title`, tv_genres.`name`;
