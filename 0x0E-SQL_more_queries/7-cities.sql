885a1b0d7ec867ef4721170888230af1b7dc3e2c
ezzedin779 <abdou.babchia64@gmail.com>
2021-03-02 21:08:08 +0100

    FOREIGN ?

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,7 +3,7 @@
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
 	id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-	FOREIGN KEY(state_id) REFERENCES states(id),
+	FOREIGN KEY(`state_id`) REFERENCES `hbtn_0d_usa`.`states`(id),
 	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL
 );
