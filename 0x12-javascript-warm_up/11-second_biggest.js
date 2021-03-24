ac1966b96788974255a38e709d93b9e3f5f3afb0
ezzedin779 <abdou.babchia64@gmail.com>
2021-03-24 08:14:57 +0100

    Second big ?

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-if (process.argv.length > 4) {
+if (process.argv.length >= 4) {
   const argument = process.argv.slice(2);
   argument.sort((x, y) => y - x);
   console.log(argument[1]);
