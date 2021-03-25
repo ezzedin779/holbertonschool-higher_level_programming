f8cac1d5cf64e6d58c30e50b00aaa017bb7e5c3d
ezzedin779 <abdou.babchia64@gmail.com>
2021-03-25 13:45:18 +0100

    going to be a father ?3

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let i;
+    for (i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const i = this.width;
+    this.width = this.height;
+    this.height = i;
+  }
+
+  double () {
+    this.width = 2 * this.width;
+    this.height = 2 * this.height;
+  }
+};
