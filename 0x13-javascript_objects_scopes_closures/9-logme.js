6bc18be5771ab3d58ed2033975228d5adec0b6e5
ezzedin779 <abdou.babchia64@gmail.com>
2021-03-25 13:47:34 +0100

    maintain it

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 exports.logMe = (function (item) {
   let c = -1;
-  return function (item) { c += 1; console.log(c + ':' + item); };
+  return function (item) { c += 1; console.log(c + ': ' + item); };
 })();
