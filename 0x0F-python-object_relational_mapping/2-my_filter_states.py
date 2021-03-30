e7297f807735810a43e746b750aaf9faecfcc678
ezzedin779 <abdou.babchia64@gmail.com>
2021-03-30 10:48:12 +0100

    That State

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Gets all states with starting N from hbtn_0e_0_usa
-Usage: ./1-filter_states.py <username> <password> <Database> """
+Usage: ./2-my_filter_states.py <username> <password> <Database> """
 import sys
 import MySQLdb
 
