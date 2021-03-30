d852d82e567313838b91153a794564f06b8e4826
ezzedin779 <abdou.babchia64@gmail.com>
2021-03-30 10:27:15 +0100

    Ntates

---
+++ 0x0F-python-object_relational_mapping/1-fileter_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Gets all states with starting N from hbtn_0e_0_usa
+Usage: ./1-filter_states.py <username> <password> <Database> """
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    s = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    q = s.cursor()
+    q.execute("SELECT * FROM `states` ORDER BY `id`")
+    for state in q.fetchall():
+        if state[1][0]:
+            print(state)
