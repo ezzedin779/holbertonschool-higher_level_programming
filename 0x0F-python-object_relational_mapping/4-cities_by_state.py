7ee3518858b439fddbd66428a9b24781cdc9c88c
ezzedin779 <abdou.babchia64@gmail.com>
2021-03-30 10:56:11 +0100

    Complicated ?

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Gets all states with starting N from hbtn_0e_0_usa
+Usage: ./1-filter_states.py <username> <password> <Database> """
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    s = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    q = s.cursor()
+    q.execute("SELECT `cities`.`id`, `cities`.`name`, `states`.`name` \
+                FROM `citites` INNER JOIN `states` \
+                ON `citites`.`state_id` = `states`.`id` \
+                ORDER BY `citites`.`id`")
+    for city in q.fetchall():
+        print(city)
