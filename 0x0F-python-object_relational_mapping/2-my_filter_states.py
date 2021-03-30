97219fbdb592fdf42b1b94316906c25f28b6fc46
ezzedin779 <abdou.babchia64@gmail.com>
2021-03-30 10:41:22 +0100

    That state ?

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Gets all states with starting N from hbtn_0e_0_usa
+Usage: ./1-filter_states.py <username> <password> <Database> """
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    s = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    q = s.cursor()
+    q.execute("SELECT * FROM `states`")
+    for state in q.fetchall():
+        if state == sys.argv[4]:
+            print(state)
