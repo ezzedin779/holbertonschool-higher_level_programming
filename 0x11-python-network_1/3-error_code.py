a06873a99ee38f96c7655f840db8f311153e6faf
ezzedin779 <abdou.babchia64@gmail.com>
2021-05-13 09:02:30 +0100

    Error code handling

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Handling errors of requests """
+import sys
+import urllib.request
+import urllib.error
+
+
+if __name__ == "__main__":
+    link = sys.argv[1]
+    req = urllib.request.Request(link)
+    try:
+        with urllib.request.urlopen(req) as response:
+            print(response.read().decode("ascii"))
+    except urllib.error.HTTPError as e:
+        print("Error code : {}".format(e.code))
