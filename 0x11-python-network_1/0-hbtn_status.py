7ac1e907998b1cd554daca20d1fd21b9ce9f1111
ezzedin779 <abdou.babchia64@gmail.com>
2021-05-13 08:21:03 +0100

    Response needed

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Fetching the intranet status """
+import urllib.request
+
+
+if __name__ == "__main__":
+    req = urllib.request.Request("https://intranet.hbtn.io/status")
+    with urllib.request.urlopen(req) as response :
+        b = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(b)))
+        print("\t- content: {}".format(b))
+        print("\t- utf8 content: {}".format(b.decode("utf-8")))
