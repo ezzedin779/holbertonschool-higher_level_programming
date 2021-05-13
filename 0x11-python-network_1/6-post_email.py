0df57017af4037dba46fc48f78657b888885f93a
ezzedin779 <abdou.babchia64@gmail.com>
2021-05-13 09:25:48 +0100

    POST request with email

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,5 +7,5 @@ import requests
 if __name__ == "__main__":
     link = sys.argv[1]
     val = {"email": sys.argv[2]}
-    req = requests.post(url, data=val)
+    req = requests.post(link, data=val)
     print(req.text)
