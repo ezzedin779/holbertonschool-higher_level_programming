4a43880fcb795a4a384550c8f674ba03619679c4
ezzedin779 <abdou.babchia64@gmail.com>
2021-05-16 14:05:56 +0100

    What's your name

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const req = require('request');
-let link = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+const link = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 req(link, function (err, res, b) {
   console.log(err || JSON.parse(b).title);
 });
