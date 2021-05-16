9124c58b68429f33716e52695be746d1e6e77a25
ezzedin779 <abdou.babchia64@gmail.com>
2021-05-16 14:25:44 +0100

    How many ?

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const req = require('request');
-req.get(process.argv[2], function (err, res, b) {
-  if (err) {
-    console.log(err);
-  }
+req.get(process.argv[2], function (error, res, b) {
   let num = 0;
+  if (error) {
+    console.log(error);
+  }
   const info = JSON.parse(b);
   for (let i = 0; info.results[i] !== undefined; i++) {
     if (info.results[i].characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
